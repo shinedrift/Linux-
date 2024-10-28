2b1bad2926bd1441cde6585a5951623edd8c244a
shinedrift <75769745+shinedrift@users.noreply.github.com>
2024-10-28 23:18:31 +0800

    Update CLBufferedFile.h

--- CLFile_Operators/CLBufferedFile.h
+++ CLFile_Operators/CLBufferedFile.h
@@ -2,12 +2,29 @@
 #define BUFFEREDFILE_H
 
 #include <iostream>
-#include <cstring>
-#include <vector>
-#include <unordered_map>
-#include <list>
 #include <fstream>
+#include "LRUCache.h"
 
-class CLBufferFile {
-    
+class BufferedFile {
+public:
+    BufferedFile(const std::string &file_path, size_t buffer_size = 64 * 1024 * 1024, 
+                 size_t block_size = 4096,  // 块大小，默认为4KB
+                 std::ios::openmode mode = std::ios::in | std::ios::out | std::ios::binary);
+    ~BufferedFile();
+
+    void write(const char *data, size_t size);
+    size_t read(char *buffer, size_t size);
+    void seek(std::streampos offset, std::ios::seekdir direction);
+
+private:
+    std::fstream file_;
+    size_t block_size_;
+    std::streampos file_pos_;
+    LRUCache cache_;
+
+    void flush();  // 将缓存的块写入文件
+    size_t getBlockIndex(std::streampos pos) const;  // 获取位置所在块的索引
+    size_t getOffsetInBlock(std::streampos pos) const;  // 获取位置在块内的偏移
 };
+
+#endif // BUFFEREDFILE_H
